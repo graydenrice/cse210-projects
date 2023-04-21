ab4578079d59601e2af4cf09581a10be89c6d191
graydenrice <graydenrice@gmail.com>
2023-04-20 20:52:30 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+            int loop = 0;
+            while (loop == 0)
+            {
+                Random randomGenerator = new Random();
+                int number = randomGenerator.Next(1,100);
+
+                Console.Write("What is the magic number? ");
+                string userinput = Console.ReadLine();
+                int guess = int.Parse(userinput);
+
+                if ( guess > number){
+                    Console.WriteLine("Lower");
+                } if (guess < number) {
+                    Console.WriteLine("Higher");
+                } if (guess == number){
+                    Console.WriteLine("You guessed right!");
+                    loop += 1;
+                }
+            }
     }
 }
