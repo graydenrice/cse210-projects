9f3305cd635c11e93eed4b8197a0a96183a4a7b7
graydenrice <graydenrice@gmail.com>
2023-04-26 20:07:17 -0600

    Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._jobTitle = "Marketing Director";
+        job2._company = "Apple";
+        job2._startYear = 2001;
+        job2._endYear = 2019;
+
+        Console.WriteLine(job2._company);
+
+        job1.DisplayJob();
+        job2.DisplayJob();
+
+        Resume resume = new Resume();
+        resume._personName = "Frankie";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        Console.WriteLine(resume._jobs[0]._jobTitle);
+        resume.DisplayResume();
     }
 }
