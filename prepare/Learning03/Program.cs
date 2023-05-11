519af3b946e8be8029a3d8a9cd0260214c183ebf
graydenrice <graydenrice@gmail.com>
2023-05-11 14:26:44 -0600

    finished learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction a = new Fraction();
+
+        Console.WriteLine(a.GetFractionString());
+        Console.WriteLine(a.GetDecimalValue());
+
+        Fraction b = new Fraction(6);
+        b.SetTop(6);
+
+        Console.WriteLine(b.GetFractionString());
+        Console.WriteLine(b.GetDecimalValue());
+
+        Fraction c = new Fraction(16, 4);
+        c.SetTop(16);
+        c.SetBottom(4);
+        
+        Console.WriteLine(c.GetFractionString());
+        Console.WriteLine(c.GetDecimalValue());
+    
     }
 }
