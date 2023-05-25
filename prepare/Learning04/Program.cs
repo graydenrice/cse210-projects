2c644da5f916579cb01ef9b2cb70e8f8277ede34
graydenrice <graydenrice@gmail.com>
2023-05-25 17:54:03 -0600

    learning4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        MathAssignment math = new MathAssignment("Grayden", "Math", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment write = new WritingAssignment("Grayden", "History", "World War II");
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
     }
 }
