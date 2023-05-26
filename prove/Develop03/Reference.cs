9873bbf8320cfd0e3b65582bf1226a357a71c91b
graydenrice <graydenrice@gmail.com>
2023-05-25 20:58:28 -0600

    new

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -4,12 +4,6 @@ public class Reference{
     private int _verse;
     private int _endVerse;
 
-    public Reference(string book, int chapter, int verse){
-        _book = book;
-        _chapter = chapter;
-        _verse = verse;
-    }
-
     public Reference(string book, int chapter, int verse, int endVerse){
         _book = book;
         _chapter = chapter;
